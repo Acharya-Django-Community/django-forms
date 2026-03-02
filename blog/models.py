a387c01286fd5443898b120f7eeabefc0286a46c
axrorback <axrorback@gmail.com>
2026-03-02 13:26:12 +0500

    Base commit for initial project

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.filter(is_active=True).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request,'home.html',context)
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request,'post_detail.html',context)
